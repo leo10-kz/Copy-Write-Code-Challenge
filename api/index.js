3a4fa959a33f84d33b5ad7e305b4b68104d99029
leonel <leitochaso10@gmail.com>
2022-07-27 21:51:24 +0200

    dotenv agregado para las variables de entorno

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const server = require('./src/server/index')
+const dotenv = require('dotenv')
 
-server.listen(3001, () => {
-    console.log('[SERVER on] : http://localhost:3001/api')
+dotenv.config();
+const port = process.env.PORT
+
+server.listen(port, () => {
+    console.log(`[SERVER on] : http://localhost:${port}/api`)
   })
   
   //* En cason de error
